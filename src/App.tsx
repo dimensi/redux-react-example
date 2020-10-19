c6bd8fac5bd2b6834e692d2a6bb416eeb0e12065
Nikita Nafranets <eddimensi@gmail.com>
2020-10-19 22:10:34 +0300

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useDispatch} from 'react-redux';
-import {Redirect, Route, Switch, useParams, useRouteMatch} from 'react-router-dom';
+import {Redirect, Route, Switch, useRouteMatch} from 'react-router-dom';
 import {defaultRepo} from './config';
 import {Issue} from './features/issue';
 import {Issues} from './features/issues';
@@ -12,12 +12,16 @@ import './styles.css';
 export default function App() {
   const error = useTypedSelector((state) => state.error);
 
-  const match = useRouteMatch('/:org/:repo')
-  const routeParams = match ? match.params as RepoMeta : defaultRepo
+  const match = useRouteMatch('/:org/:repo');
+  const routeParams = match ? (match.params as RepoMeta) : defaultRepo;
   const dispatch = useDispatch();
   const repoMeta = useTypedSelector((state) => state.repo);
 
-  if (isMissmatchRepo(repoMeta, routeParams) && routeParams.repo && routeParams.org) {
+  if (
+    isMissmatchRepo(repoMeta, routeParams) &&
+    routeParams.repo &&
+    routeParams.org
+  ) {
     dispatch(setRepo(routeParams));
   }
 
